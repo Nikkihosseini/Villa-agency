bf6c7c276a1aacf212e981ff029d4d8d9203b229
Nikkihosseini <niekal00@gmail.com>
2025-02-03 12:30:04 +0330

    add HeaderBanner and swiper js and Customize swiper js styles for Navigation and Pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './index.css'
 import Header from './Header/Header.jsx'
+import HeaderBanner from './Header/HeaderBanner.jsx'
 
 export default function App() {
 
   return (
     <>
      <Header/>
+     <HeaderBanner/>
     </>
   )
 }
