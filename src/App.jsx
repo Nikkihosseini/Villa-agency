d398c3d16dfd7bd398f293d5b7d22e76a9201d3f
Nikkihosseini <niekal00@gmail.com>
2025-02-09 18:14:37 +0330

    add title (best deal)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './Components/Home/Header/Header.jsx'
 import HeaderBanner from './Components/Home/Header/HeaderBanner.jsx'
 import Featured from './Components/Home/Featured/Featured.jsx'
 import VideoView from './Components/Home/VideoView/VideoView.jsx'
+import BestDeal from './Components/Home/BestDeal/BestDeal.jsx'
 
 export default function App() {
 
@@ -12,6 +13,7 @@ export default function App() {
      <HeaderBanner/>
      <Featured/>
      <VideoView/>
+     <BestDeal/>
     </>
   )
 }
---
+++ src/Components/Home/BestDeal/BestDeal.jsx
@@ -0,0 +1,20 @@
+import '../../../index.css'
+
+
+export default function BestDeal(){
+    return(
+        <>
+            <div className='bg-gray-50 h-[110vh]'>
+                <div className='container mx-auto px-4 xl:px-20'>
+                   <div className='py-12 md:py-26'>
+                        <div className='md:mb-0 mb-4'>
+                            <h4 className='line-before text-red-500 relative font-bold uppercase ml-2.5 mb-4'>Best Deal</h4>
+                            <h3 className='font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl w-[210px] md:max-w-[400px]'>Find Your Best Deal Right Now!
+                            </h3>
+                     </div>
+                   </div>
+                </div>
+            </div>
+        </>
+    )
+}
