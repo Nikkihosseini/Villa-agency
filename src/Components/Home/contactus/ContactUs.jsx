92074798338dd7a3f2346536de8638b8cec50ca2
Nikkihosseini <niekal00@gmail.com>
2025-02-18 11:38:01 +0330

    add Responsive code for ContactUs Section

--- src/Components/Home/contactus/ContactUs.jsx
+++ src/Components/Home/contactus/ContactUs.jsx
@@ -20,14 +20,14 @@ export default function ContactUs(){
                                 <div className='flex items-center text-start w-full lg:w-1/2 gap-x-6 shadow-normal rounded-[0.8rem] p-8 bg-white'>
                                 <img src="./public/images/icon/phone-icon.png" alt="phone-icon"/>
                                 <div className='flex flex-col'>
-                                    <span className='font-bold text-lg xl:text-xl'>010-020-0340</span>
+                                    <span className='font-bold text-base sm:text-lg xl:text-xl'>010-020-0340</span>
                                     <span className='text-gray-400'>Phone Number</span>
                                 </div>
                                 </div>
                                 <div className='flex items-center text-start w-full lg:w-1/2 gap-x-6 shadow-normal rounded-[0.8rem] p-8 bg-white '>
                                 <img src="./public/images/icon/email-icon.png" alt="phone-icon"/>
                                 <div className='flex flex-col'>
-                                    <span className='font-bold text-lg xl:text-xl'>info@villa.co</span>
+                                    <span className='font-bold text-base sm:text-lg xl:text-xl'>info@villa.co</span>
                                     <span className='text-gray-400'>Business Email</span>
                                 </div>
                                 </div>
