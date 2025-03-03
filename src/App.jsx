4733b1c28e59e0ebc362951d281cdf3e6975c7f2
Nikkihosseini <niekal00@gmail.com>
2025-03-03 11:44:49 +0330

    add react router dom to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
 import './index.css'
-import Header from './Components/Home/Header/Header.jsx'
-import HeaderBanner from './Components/Home/Header/HeaderBanner.jsx'
-import Featured from './Components/Home/Main/Featured/Featured.jsx'
-import VideoView from './Components/Home/Main/VideoView/VideoView.jsx'
-import BestDeal from './Components/Home/Main/BestDeal/BestDeal.jsx'
-import Properties from './Components/Home/Main/Properties/Properties.jsx'
-import ContactUs from './Components/Home/Main/contactus/ContactUs.jsx'
-import Footer from './Components/Home/Footer/Footer.jsx'
+import Home from './Components/Home/Home.jsx'
+import { BrowserRouter as Router , Routes , Route } from 'react-router'
 
 
 export default function App() {
 
   return (
     <>
-     <Header/>
-     <HeaderBanner/>
-     <Featured/>
-     <VideoView/>
-     <BestDeal/>
-     <Properties/>
-     <ContactUs/>
-     <Footer/>
+      <Home/>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/Properties' element={<Properties/>} />
+          <Route path='/PropertyDetails' element={<PropertyDetails/>} />
+          <Route path='/ContactUs' element={<ContactUs/>} />
+        </Routes>
+      </Router>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+const flowbite = require("flowbite-react/tailwind");
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss()
+    tailwindcss(),
+    flowbite.plugin(),
+  ],
+  content: [
+    flowbite.content(),
   ],
 })
