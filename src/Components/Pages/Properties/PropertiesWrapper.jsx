557ac098e574d104a4e60ac16c4332c8db9b43ec
Nikkihosseini <niekal00@gmail.com>
2025-03-10 14:51:33 +0330

    add pagination btn

--- src/Components/Pages/Properties/PropertiesWrapper.jsx
+++ src/Components/Pages/Properties/PropertiesWrapper.jsx
@@ -115,7 +115,7 @@ export default function PropertiesWrapper(){
     ]
 
     const [active , setActive] = useState('')
-
+   
     function activeBtn(event){
         setActive(event.target.id)
     }
@@ -123,7 +123,7 @@ export default function PropertiesWrapper(){
     return(
         <Fragment>
             <div className="container mx-auto px-4 xl:px-20">
-                <div className="flex flex-col items-center justify-center mt-30 gap-y-20">
+                <div className="flex flex-col items-center justify-center my-30 gap-y-20">
                     <div className='flex items-center justify-center gap-x-4 text-white font-semibold flex-wrap sm:flex-nowrap  gap-y-3 sm:gap-y-0'>
                         <button onClick={activeBtn} id="1" className={`bg-zinc-950 ${active === "1" ? 'active' : 'hover:text-red-500'} rounded-md w-[90px] h-[35px] sm:w-[100px] sm:h-[45px] cursor-pointer tracking-wide text-sm md:text-base transition-all`}>Show All</button>
                         <button onClick={activeBtn} id="2" className={`${active === "2" ? 'active' : 'hover:text-red-500'} bg-zinc-950 rounded-md w-[110px] h-[35px] sm:w-[130px] sm:h-[45px] cursor-pointer tracking-wide transition-all text-sm md:text-base`}>Appartment</button>
@@ -141,7 +141,14 @@ export default function PropertiesWrapper(){
                         <PropertiesBox {...Data[7]}/>
                         <PropertiesBox {...Data[8]}/>
                     </div> 
-                    <div></div>
+                    <div>
+                        <div className="flex items-center justify-center gap-x-4">
+                            <button className="w-[40px] h-[40px] rounded-full bg-zinc-950 text-white cursor-pointer">1</button>
+                            <button className="w-[40px] h-[40px] rounded-full bg-zinc-950 text-white cursor-pointer">2</button>
+                            <button className="w-[40px] h-[40px] rounded-full bg-zinc-950 text-white cursor-pointer">3</button>
+                            <button className="flex items-center justify-center leading-[34px] w-[40px] h-[40px] rounded-full bg-zinc-950 text-white cursor-pointer text-xl">&raquo;</button>
+                        </div>
+                    </div>
                 </div> 
             </div>
         </Fragment>
