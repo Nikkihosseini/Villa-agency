766cf9ef98e0d4c029c8290becad8099d2efd11f
Nikkihosseini <niekal00@gmail.com>
2025-03-10 11:25:11 +0330

    add btn for PropertiesWrapper

--- src/Components/Pages/Properties/Properties.jsx
+++ src/Components/Pages/Properties/Properties.jsx
@@ -1,11 +1,13 @@
 import Header from '../../Header/Header.jsx'
 import BannerPages from '../../BannerPages/BannerPages.jsx'
+import PropertiesWrapper from './PropertiesWrapper.jsx'
 
 export default function Properties(){
     return(
         <>
             <Header/>
             <BannerPages pageName='Properties'/>
+            <PropertiesWrapper/>
         </>
     )
 }
