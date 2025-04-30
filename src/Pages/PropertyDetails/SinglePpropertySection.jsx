a24b32e973f872dcf0fb09382114b150bb5e72fd
Nikkihosseini <niekal00@gmail.com>
2025-04-30 22:15:59 +0330

    add SinglePpropertySection section

---
+++ src/Pages/PropertyDetails/SinglePpropertySection.jsx
@@ -0,0 +1,10 @@
+import InfoTable from '../../Components/Info-table/Info-table-icon'
+
+
+export default function SinglePpropertySection(){
+    return(
+        <>
+            <InfoTable/>
+        </>
+    )
+}
