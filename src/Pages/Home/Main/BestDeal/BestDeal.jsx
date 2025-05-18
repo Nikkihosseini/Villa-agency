c4f80e6d4b65ad00c1f9fd1d005f6b0d72b5dfea
Nikkihosseini <niekal00@gmail.com>
2025-05-18 15:09:44 +0330

    edit BestDeal style for btn hover

--- src/Pages/Home/Main/BestDeal/BestDeal.jsx
+++ src/Pages/Home/Main/BestDeal/BestDeal.jsx
@@ -16,8 +16,8 @@ export default function BestDeal(){
                          </div>
                          <div className='flex items-center justify-center gap-x-4 text-white font-semibold flex-wrap sm:flex-nowrap  gap-y-3 sm:gap-y-0 sm:w-[480px]'>
                             <button className='bg-red-500 rounded-md w-[130px] h-[35px] sm:w-[150px] sm:h-[50px] cursor-pointer tracking-wide text-base sm:text-lg'>Appartment</button>
-                            <button className='bg-zinc-950 rounded-md w-[130px] h-[35px] sm:w-[150px] sm:h-[50px] cursor-pointer tracking-wide text-base sm:text-lg sm:hover:w-[149px] sm:hover:h-[49px] transition-all'>Villa House</button>
-                            <button className='bg-zinc-950 rounded-md w-[130px] h-[35px] sm:w-[150px] sm:h-[50px] cursor-pointer tracking-wide text-base sm:text-lg sm:hover:w-[149px] sm:hover:h-[49px] transition-all'>Penthouse</button>
+                            <button className='bg-zinc-950 rounded-md w-[130px] h-[35px] sm:w-[150px] sm:h-[50px] cursor-pointer tracking-wide text-base sm:text-lg hover:bg-red-500 transition-all'>Villa House</button>
+                            <button className='bg-zinc-950 rounded-md w-[130px] h-[35px] sm:w-[150px] sm:h-[50px] cursor-pointer tracking-wide text-base sm:text-lg hover:bg-red-500 transition-all'>Penthouse</button>
                          </div> 
                      </div>
                      <div className='flex flex-col lg:flex-row gap-y-5 lg:gap-y-0 items-center justify-around gap-x-5 lg:h-[414px]'>
