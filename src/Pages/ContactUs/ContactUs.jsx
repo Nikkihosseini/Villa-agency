f501bc7270c7f887b95e66b22e8160ba09808c44
Nikkihosseini <niekal00@gmail.com>
2025-05-18 15:26:24 +0330

    create ContactUs page

---
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -0,0 +1,14 @@
+import Header from '../../Components/Header/Header.jsx'
+import BannerPages from '../../Components/BannerPages/BannerPages.jsx'
+import Footer from "../../Components/Footer/Footer"
+
+
+export default function ContactUs(){
+    return(
+        <>
+            <Header/>
+            <BannerPages pageName='Contact Us'/>
+            <Footer/>
+        </>
+    )
+}
